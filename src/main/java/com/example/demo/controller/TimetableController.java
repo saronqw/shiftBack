805a73319bf5fb0b297340e5f78df8cfc4ada84b
threelephant <threelephant@gmail.com>
2020-02-04 22:05:59 +0700

    Delete odd function from TimetableController

--- src/main/java/com/example/demo/controller/TimetableController.java
+++ src/main/java/com/example/demo/controller/TimetableController.java
@@ -47,30 +47,4 @@ public class TimetableController {
     public ReservEntity get(@PathVariable(name = "id") Long id) {
         return reservingService.get(id);
     }
-
-    @RequestMapping(method = RequestMethod.GET, path = "/categories/trainings", produces = "application/json")
-    public TrainingList[] getTrainingsList() {
-        return TrainingList.values();
-    }
-
-    @RequestMapping(method = RequestMethod.GET, path = "/categories/sports", produces = "application/json")
-    public SportList[] getSportList() {
-        return SportList.values();
-    }
-
-    @RequestMapping(method = RequestMethod.GET, path = "/categories/spa", produces = "application/json")
-    public SPAList[] getSPAList() {
-        return SPAList.values();
-    }
-
-    @RequestMapping(method = RequestMethod.GET, path = "/categories/pool", produces = "application/json")
-    public PoolList[] getPoolList() {
-        return PoolList.values();
-    }
-
-    @RequestMapping(method = RequestMethod.GET, path = "/categories/bar", produces = "application/json")
-    public BarList[] getBarList() {
-        return BarList.values();
-    }
-
 }

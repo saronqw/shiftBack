991b6b313583ec30c68710442bd00b6f427fc727
Ivan Boyko <saronqw@gmail.com>
2020-02-06 17:00:56 +0700

    Fix CORS

---
+++ src/main/java/com/example/demo/filters/CorsFilter.java
@@ -0,0 +1,23 @@
+package com.example.demo.filters;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Expose-Headers", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/example/demo/filters/Filters.java
@@ -0,0 +1,18 @@
+package com.example.demo.filters;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+@Configuration
+public class Filters {
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean() {
+        FilterRegistrationBean<CorsFilter> corsFilter = new FilterRegistrationBean<>();
+        corsFilter.setOrder(Ordered.LOWEST_PRECEDENCE);
+        corsFilter.setFilter(new CorsFilter());
+        corsFilter.addUrlPatterns("/*");
+        return corsFilter;
+    }
+}
